7f5885c44bf7ff761ef04f3f3c680d505ef53b49
Sanjeeban2411 <sanju.sanjeeban.sp@gmail.com>
2023-04-09 20:33:13 +0530

    login refact

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -25,54 +25,68 @@ function Login() {
   };
 
   return (
-    <div className="container">
-      <div classname="header d-flex flex-row" style={{display:"flex"}}>
-        <div className="imagecontainer">
-        <img src={InsuranceLogo} alt="Insurance Website Logo" style={{width:"100px", height:"auto"}} />
+    <div className="page">
+      <div className="container">
+        <div
+          classname="header d-flex flex-row"
+          style={{
+            "display": "flex",
+            "justify-content": "space-around",
+            "align-items": "center",
+            "text-align": "center",
+          }}
+        >
+          <div className="imagecontainer">
+            <img
+              src={InsuranceLogo}
+              alt="Insurance Website Logo"
+              style={{ width: "100px", height: "auto" }}
+            />
+          </div>
+          <div>
+            <h1>Randi Insurance</h1>
+          </div>
         </div>
-        <div>
-          <h1>Randi Insurance</h1>
-        </div>
-      </div>
 
-      <div className="form-container">
-        <h2>Login</h2>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="username-email">Username or Email:</label>
-          <input
-            type="text"
-            id="username-email"
-            value={username}
-            onChange={handleUsernameChange}
-            required
-          />
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={handlePasswordChange}
-            required
-          />
-          <label htmlFor="remember-me">Remember Me</label>
-          <input
-            type="checkbox"
-            id="remember-me"
-            checked={rememberMe}
-            onChange={handleRememberMeChange}
-          />
-          <input type="submit" value="Login" />
-        </form>
-        <div className="links">
-          <a href="#">Forgot your password? Click here.</a>
+        <div className="form-container">
+          <h2>Login</h2>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="username-email">Username or Email:</label>
+            <input
+              type="text"
+              id="username-email"
+              value={username}
+              onChange={handleUsernameChange}
+              required
+            />
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={handlePasswordChange}
+              required
+            />
+            <label htmlFor="remember-me">Remember Me</label>
+            <input
+              type="checkbox"
+              id="remember-me"
+              checked={rememberMe}
+              onChange={handleRememberMeChange}
+            />
+            <input type="submit" value="Login" />
+          </form>
+          <div className="links">
+            <a href="#">Forgot your password? Click here.</a>
+            <p>
+              Not a Member Yet? <a href="#">Sign Up</a>
+            </p>
+          </div>
           <p>
-            Not a Member Yet? <a href="#">Sign Up</a>
+            By logging in, you agree to our <a href="#">Terms and Conditions</a>{" "}
+            and <a href="#">Privacy Policy</a>.
           </p>
         </div>
-        <p>
-          By logging in, you agree to our <a href="#">Terms and Conditions</a>{" "}
-          and <a href="#">Privacy Policy</a>.
-        </p>
       </div>
     </div>
   );
